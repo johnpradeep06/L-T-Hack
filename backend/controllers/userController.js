539f52ebed5962756ab37dfcea0dcea4a4bd82d0
johnpradeep06 <johnpradeep100@gmail.com>
2025-02-15 16:05:30 +0530

    deleted hacktivate folder

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,11 +5,13 @@ exports.signup = async (req, res) => {
     const { username, password } = req.body;
     
     if (!username || !password) {
+        console.log('Username and password are required');
         return res.status(400).json({ message: 'Username and password are required' });
     }
     
     const hashedPassword = await bcrypt.hash(password, 10);
     users.push({ username, password: hashedPassword });
+    console.log(`User Registered: ${username}`);
     res.status(201).json({ message: 'User registered successfully' });
 };
 
@@ -18,13 +20,15 @@ exports.login = async (req, res) => {
     
     const user = users.find(user => user.username === username);
     if (!user) {
+        console.log(`User not found: ${username}`);
         return res.status(400).json({ message: 'Invalid username or password' });
     }
     
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
+        console.log(`Invalid password for user: ${username}`);
         return res.status(400).json({ message: 'Invalid username or password' });
     }
-    
+    console.log(`User logged in: ${username}`);
     res.json({ message: 'Login successful' });
 };
